962d32457c14ae95a281450db2bf8da7a729c81c
struggler5 <dima.vleju@gmail.com>
2026-02-20 21:34:08 +0200

    added 2 more pages including modifying the DB

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template 
+
+app = Flask(__name__)
+
+@app.route('/')
+def Transactions():
+
+    return render_template("Transactions.html")
+
+@app.route('/DB')
+def DB():
+    return render_template("DB.html")
+
+@app.route('/DB/item')
+def DB_item():
+    return render_template("DB_item.html")
+
+@app.route('/Stock')
+def Stock():
+    return render_template("Stock.html")
+app.run(debug=True, host="0.0.0.0", port=5000)
