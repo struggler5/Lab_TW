decce5e76329ff2de3dc9c646605ca5975942dbd
struggler5 <dima.vleju@gmail.com>
2026-03-01 14:07:54 +0200

    added alerts

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+function OnDeleate() {
+    let decision = confirm("Are you sure you want to delete this?");
+
+    if (decision) {
+        console.log("User clicked Yes!");
+        // Put your 'Yes' logic here
+    } else {
+        console.log("User clicked No.");
+    }
+}
+
+
+function Construction(){
+    alert("Website still under construction! ");
+}
+
+
